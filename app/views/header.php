9b2862c09e0b6e4fa00a56719c9bd08a61f050eb
David Seaton <davidseaton@trevor.local>
2013-11-12 17:46:52 +0000

    added markup files, fixed config

--- app/views/header.php
+++ app/views/header.php
@@ -1,27 +1,24 @@
 <!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="https://www.facebook.com/2008/fbml">
+<html lang="en">
   <head>
     <meta charset="utf-8">
-    <title><?= $title ?> | Aegis</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?= $description ?>">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="path/to/favicon.png">
 
-    <!-- Los styles -->    
-    <link href="<?= site_url('assets/css/bootstrap.css') ?>" rel="stylesheet">
-    <link href="<?= site_url('assets/css/responsive.css') ?>" rel="stylesheet">
-    <link href="<?= site_url('assets/css/animate.css') ?>" rel="stylesheet" type="text/css">
-    <link href="<?= site_url('assets/css/app.css') ?>" rel="stylesheet">       
-    <script src="<?= site_url('assets/js/library/css_browser_selector.js') ?>" type="text/javascript"></script>
+    <title>Starter Template for Bootstrap</title>
 
-    <!-- el HTML5 shimo, por IE6-8 supporto de HTML5 elementos -->
+    <!-- Bootstrap core CSS -->
+    <link href="<?= site_url('assets/css/base.css') ?>" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
-
-    <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="<?= site_url('/assets/images/favicon.ico') ?>">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= site_url('assets/ico/apple-touch-icon-114-precomposed.png')?>">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= site_url('assets/ico/apple-touch-icon-72-precomposed.png') ?>">
-    <link rel="apple-touch-icon-precomposed" href="<?= site_url('assets/ico/apple-touch-icon-57-precomposed.png') ?>">
-    <?= render_styles() ?>
-</head>
+  </head>
--- app/views/subhead.php
+++ app/views/subhead.php
@@ -1,25 +1,23 @@
-<body <?= body_classes() ?>>
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container-fluid">
-        <a class="brand" href="#"><img src="<?= site_url('/assets/images/logo.png')?>"/></a>
-    	<ul class="nav pull-right">                        
-    	<? if(logged_in()) { ?>                                                                 
-    	    <li><a href="<?= site_url('/admin/events/browse') ?>">Events</a></li>
-    	    <li><a href="<?= site_url('/admin/bands/browse') ?>">Bands</a></li>
-    	    <li><a href="<?= site_url('/admin/venues/browse') ?>">Venues</a></li>
-    	    <li><a href="<?= site_url('/admin/regions/browse') ?>">Regions</a></li>
-    	    <li><a href="<?= site_url('/admin/genres/browse') ?>">Genres</a></li>
-    	    <li><a href="<?= site_url('/user/users/browse') ?>">Users</a></li>
-    	    <li><a href="<?= site_url('/admin/reports/events') ?>">Reports</a></li>
-    	    <li><a href="<?= site_url('/admin/cron') ?>" target="_BLANK">Sync Events</a></li>
-    	    <li><a href="<?= site_url('/user/logout') ?>">Logout</a></li> 
-    	<? } ?>
-    	</ul>
+  <body>
+
+    <div class="navbar navbar-inverse" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
         </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#link">Somewhere Else</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
       </div>
     </div>
-    
-    <?php $this->load->view('notifications') // display the notitification partial ?>
-    
-    <div class="main">
+
+    <div class="container">
