c23e0a4865a3e9fb80c149b9edce2a71bdbb0895
Julio Taylor <julio@bigspring.co.uk>
2013-11-13 09:45:35 +0000

    improved header & notifications markup

--- app/views/header.php
+++ app/views/header.php
@@ -8,7 +8,7 @@
     <meta name="author" content="">
     <link rel="shortcut icon" href="path/to/favicon.png">
 
-    <title>Starter Template for Bootstrap</title>
+    <title>Welcome to The Machine</title>
 
     <!-- Bootstrap core CSS -->
     <link href="<?= site_url('assets/css/base.css') ?>" rel="stylesheet">
--- app/views/notifications.php
+++ app/views/notifications.php
@@ -2,7 +2,7 @@
 	<? if($this->messages->count('error') > 0) { // if we have error messages?>
 		<? foreach($this->messages->get('error') AS $message) { ?>
     			<div class="alert alert-error">
-    				<button type="button" class="close" data-dismiss="alert">×</button>
+    				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
     				<?= $message; // display ui notifications ?>			
     			</div>
 		<? } // endforeach ?>
@@ -11,7 +11,7 @@
 	<? if($this->messages->count('success') > 0) { // if we have success messages?>
 		<? foreach($this->messages->get('success') AS $message) { ?>
     			<div class="alert alert-success animate bounceInDown">
-    			    <button type="button" class="close" data-dismiss="alert">×</button>
+    			    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
     				<?= $message; // display ui notifications ?>			
     			</div>
 		<? } // endforeach ?>
@@ -20,7 +20,7 @@
 	<? if($this->messages->count('info') > 0) { // if we have info messages?>
 		<? foreach($this->messages->get('info') AS $message) { ?>
     			<div class="alert alert-info">
-    			    <button type="button" class="close" data-dismiss="alert">×</button>
+    			    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
     				<?= $message; // display ui notifications ?>			
     			</div>
 		<? } // endforeach ?>
