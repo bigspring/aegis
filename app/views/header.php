64b5304f63de93a9bc31ac2f98034f3bbe9fc6c2
Julio Taylor <julio@bigspring.co.uk>
2013-11-13 10:07:26 +0000

    improved header scripts

--- app/views/header.php
+++ app/views/header.php
@@ -16,7 +16,7 @@
     <!-- Modernizer -->
     <link href="<?= site_url('assets/js/library/modernizer.min.js') ?>" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- Respond.js for IE8 support of media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
