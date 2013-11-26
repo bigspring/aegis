1e6abcc43cd4feb30ce3693e7c367db6233aa743
David Seaton <davidseaton@trevor.local>
2013-11-26 11:17:37 +0000

    added user_groups default row insertion
    insert SQL to create 'user' and 'admin' user types in user_groups table

--- app/modules/user/installation/users_setup.sql
+++ app/modules/user/installation/users_setup.sql
@@ -6,6 +6,12 @@ CREATE TABLE `ae_user_groups` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+-- Insert default data for TABLE 'ae_user_groups'
+INSERT INTO `ae_user_groups` (`id`, `title`, `description`)
+VALUES
+	(1, 'Administrator', 'Administrative users that have access to the back end'),
+	(100, 'User', 'General front end users with no access to the back end');
+
 -- Create syntax for TABLE 'ae_user_logins'
 CREATE TABLE `ae_user_logins` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -32,5 +38,6 @@ CREATE TABLE `ae_users` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+-- Insert default data for TABLE 'ae_users'
 INSERT INTO `ae_users` (`id`, `email`, `firstname`, `lastname`, `password`, `user_group_id`, `token`, `identifier`, `enabled`, `created`, `createdby`, `modified`, `modifiedby`)
 VALUES (1,'aegis@bigspring.co.uk','change','me','32910af88d6192244308dd74451ad7e3a002ea2a5ad2b7abb10fc22c4200b503',1,'','',1,'0000-00-00 00:00:00',1,'0000-00-00 00:00:00',1);
