9b31a4229cfb322fee81c243571b881259ce1feb
Julio Taylor <julio@bigspring.co.uk>
2014-02-03 11:18:25 +0000

    cleanup up password reset form, added html5 markup
    #7

--- app/modules/user/views/forgot/forgot.php
+++ app/modules/user/views/forgot/forgot.php
@@ -1,17 +1,11 @@
-<div class="row">
-	<div class="col-sm-6 col-sm-offset-3">
+<header class="page-header">
+	<h1>Password Recovery</h1>
+	<p class="lead">Enter your email address below to reset your password.</p>
+</header>
 
-		<header class="page-header">
-			<h1>Password Recovery</h1>
-			<p class="lead">Enter your email address below to reset your password.</p>
-		</header>
-	
-		<section class="password-reset">
-		<?= form_open('', array('class' => 'form')); ?>	
-			<input type="text" value="" name="email" placeholder="Email Address" class="form-control"><br/>
-			<?= form_submit(array('name' => 'submit', 'id' => 'submit', 'class' => 'btn btn-primary', 'value' => 'Reset my password')) ?> <a class="btn btn-link" href="<?= site_url('user/login')?>">Already have a login?</a>
-		<?= form_close() ?>
-		</section>
-
-	</div>
-</div>
+<section class="password-reset">
+<?= form_open('', array('class' => 'form')); ?>	
+	<input type="text" value="" name="email" placeholder="Email Address" class="form-control"><br/>
+	<?= form_submit(array('name' => 'submit', 'id' => 'submit', 'class' => 'btn btn-primary', 'value' => 'Reset my password')) ?> <a class="btn btn-link" href="<?= site_url('user/login')?>">Already have a login?</a>
+<?= form_close() ?>
+</section>
--- app/modules/user/views/forgot/reset.php
+++ app/modules/user/views/forgot/reset.php
@@ -1,7 +1,13 @@
-<h1>Reset Password</h1>
-<?php $this->load->view('notifications') // display the notitification partial ?>
-<?= form_open(''); ?>
-<?= form_password('password', '', 'Password*', 'class="col-md-5" placeholder="Enter a password" data-validation-required-message="Please enter your password" required', $user->error->password) ?>
-<?= form_password('confirmpassword', '', 'Confirm Password*', 'class="col-md-5" placeholder="Re-enter password" data-validation-required-message="Please enter your first name" required data-validation-match-match="password"', $user->error->confirmpassword) ?>                
-<?= form_submit(array('name' => 'submit', 'id' => 'submit', 'class' => 'btn btn-large btn-success pull-right', 'value' => 'Reset')) ?>
-<?= form_close() ?>   
+<header class="page-header" role="banner">
+	<h1>Reset Password</h1>
+</header>
+
+<main class="password-reset-form" role="main">
+	<?= form_open('', array('class' => 'form')); ?>	
+	<?= form_password('password', '', 'Password*', 'class="form-control" placeholder="Enter a password" data-validation-required-message="Please enter your password" required', $user->error->password) ?>
+	<?= form_password('confirmpassword', '', 'Confirm Password*', 'class="form-control" placeholder="Re-enter password" data-validation-required-message="Please enter your first name" required data-validation-match-match="password"', $user->error->confirmpassword) ?>                
+	<hr />
+	<?= form_submit(array('name' => 'submit', 'id' => 'submit', 'class' => 'btn btn-primary', 'value' => 'Reset password')) ?>
+	<a href="<?= site_url('user/login')?>" class="btn btn-link">Cancel password reset</a>
+	<?= form_close() ?>   
+</main>
