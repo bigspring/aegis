4ac702a7d27c448ffcd76c3ef344a453faf9fa64
Julio Taylor <julio@bigspring.co.uk>
2014-02-03 11:20:21 +0000

    cleanup up failed-verify markup

--- app/modules/user/views/failed_verify.php
+++ app/modules/user/views/failed_verify.php
@@ -1,2 +1,10 @@
-<h1>Email Verification</h1>
-<p>Sorry, we were unable to verify your email address.  Has your account already been verified?</p>
+<header class="page-header" role="banner">
+	<h1>Email Verification</h1>	
+</header>
+
+<main role="main">
+	<div class="alert alert-danger">
+		<p>Sorry, we were unable to verify your email address.  Has your account already been verified?</p>	
+	</div>
+</main>
+
--- app/modules/user/views/forgot/forgot.php
+++ app/modules/user/views/forgot/forgot.php
@@ -1,11 +1,11 @@
-<header class="page-header">
+<header class="page-header" role="banner">
 	<h1>Password Recovery</h1>
 	<p class="lead">Enter your email address below to reset your password.</p>
 </header>
 
-<section class="password-reset">
+<main class="password-reset" role="main">
 <?= form_open('', array('class' => 'form')); ?>	
 	<input type="text" value="" name="email" placeholder="Email Address" class="form-control"><br/>
 	<?= form_submit(array('name' => 'submit', 'id' => 'submit', 'class' => 'btn btn-primary', 'value' => 'Reset my password')) ?> <a class="btn btn-link" href="<?= site_url('user/login')?>">Already have a login?</a>
 <?= form_close() ?>
-</section>
+</main>
