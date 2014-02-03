b4b678926a7614d0689952bff1570868b27977cc
Julio Taylor <julio@bigspring.co.uk>
2014-02-03 11:32:05 +0000

    cleaned up js assets
    closes #12

--- app/views/header.php
+++ app/views/header.php
@@ -4,8 +4,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
     <link rel="shortcut icon" href="path/to/favicon.png">
 
     <title>Welcome to The Machine</title>
