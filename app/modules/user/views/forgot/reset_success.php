46e3ed51b0d8b1395dd5d52c60efceddeeb9d2e7
Julio Taylor <julio@bigspring.co.uk>
2014-02-03 10:10:41 +0000

    cleanup of users/reset_success
    #7

--- app/modules/user/views/forgot/reset_success.php
+++ app/modules/user/views/forgot/reset_success.php
@@ -1,24 +1,3 @@
-<div id="details" class="header-wrap">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="page-header">
-                    <h1>Password Reset Successfully</h1>
-                </div> <!-- /.page-header -->
-            </div> <!-- /.col-md-12 -->
-            <div class="col-md-3">
-            </div>
-        </div> <!-- /.row -->
-    </div>
-</div>
-<div class="container">
-        <div id="main">
-                <div id="content">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <p>Your password has now been reset.  Please <a href="<?= site_url('user/login') ?>">click here</a> to login.</p>                            
-                        </div>
-                    </div>  
-                </div> <!-- /#content -->
-        </div> <!-- /#main -->
-</div> <!-- /.container -->
+<h1>Password Reset Successfully</h1>
+<p>Your password has now been reset.
+<a class="btn btn-default" href="<?= site_url('user/login') ?>">Click here to log in</a>
