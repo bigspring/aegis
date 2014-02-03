9239dd63295cdda5789654f8a02f1c673d1c720b
Julio Taylor <julio@bigspring.co.uk>
2014-02-03 10:09:34 +0000

    cleanup of /users/reset-not-found
    #7

--- app/modules/user/views/forgot/reset_not_found.php
+++ app/modules/user/views/forgot/reset_not_found.php
@@ -1,24 +1,2 @@
-<div id="details" class="header-wrap">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="page-header">
-                    <h1>Password Reminder Sent</h1>
-                </div> <!-- /.page-header -->
-            </div> <!-- /.col-md-12 -->
-            <div class="col-md-3">
-            </div>
-        </div> <!-- /.row -->
-    </div>
-</div>
-<div class="container">
-        <div id="main">
-                <div id="content">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <p>We were unable to find a user that matched your password reset request.  Please try again.</p>                            
-                        </div>
-                    </div>  
-                </div> <!-- /#content -->
-        </div> <!-- /#main -->
-</div> <!-- /.container -->
+<h1>Password Reminder Sent</h1>
+<p>We were unable to find a user that matched your password reset request.  Please try again.</p>
