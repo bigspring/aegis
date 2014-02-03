fd16c380ca0bd80888a132436e156c3d0c95ef36
Julio Taylor <julio@bigspring.co.uk>
2014-02-03 09:45:22 +0000

    removed redundant markup from user/verify
    addresses #7

--- app/modules/user/views/verify.php
+++ app/modules/user/views/verify.php
@@ -1,23 +1,3 @@
-<div class="header-wrap">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-9">
-                <div class="page-header">
-                    <h1>Email Verification</h1>
-                </div> <!-- /.page-header -->
-            </div> <!-- /.col-md-12 -->
-        </div> <!-- /.row -->
-    </div>
-</div>
-<div class="container">
-    <div id="main">
-        <div id="content">
-            <div class="row">
-                <div class="col-md-12">
-                    <p>Thank you, your email address has now been verified.</p>
-                    <p>You can now <a href="<?= site_url('user/login') ?>">login</a> to the website.</p>
-                </div> <!-- /.col-md-12 -->
-            </div> <!-- /.row -->
-        </div> <!-- /#content -->
-    </div> <!-- /#main -->
-</div> <!-- /.container -->
+<h1>Email Verification</h1>
+<p>Thank you, your email address has now been verified.</p>
+<p>You can now <a href="<?= site_url('user/login') ?>">login</a> to the website.</p>
