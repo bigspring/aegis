9ed1381b2369f7e2497c8e947c89a562e246d484
Julio Taylor <julio@bigspring.co.uk>
2014-02-03 10:12:35 +0000

    #7 cleaned up users/login

--- app/modules/user/views/login/login.php
+++ app/modules/user/views/login/login.php
@@ -1,13 +1,8 @@
 <?php $this->load->view('notifications') // display the notitification partial ?>
-<h4>Access to this area is restricted.  Please log in to continue.</h4>
-
+<div class="alert alert-warning">Access to this area is restricted.  Please log in to continue.</div>
 <h1>Login</h1>
-<div id="login">			
-
-<hr/>
 <?= form_open(''); ?>
 <input type="text" value="" name="username" placeholder="Enter email Address" class="col-md-5"><br/>
 <input type="password" value="" name="password" placeholder="Enter password" class="col-md-5"><br/>
 <?= form_submit(array('name' => 'submit', 'id' => 'submit', 'class' => 'btn btn-default', 'value' => 'Login')) ?>			
 <?= form_close() ?>						
-</div> <!--/#login-->
