d2df429d4cf954669b30ab11a774fb5823d00d42
Julio Taylor <julio@bigspring.co.uk>
2014-02-03 10:12:55 +0000

    #7 cleaned up users/add

--- app/modules/user/views/users/add.php
+++ app/modules/user/views/users/add.php
@@ -1,10 +1,2 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="page-header">
-                <h1>Add User</h1>
-            </div>
-            <?php $this->load->view('user/users/form'); ?>
-        </div>    
-    </div>
-</div>
+<h1>Add User</h1>
+<?php $this->load->view('user/users/form'); ?>
