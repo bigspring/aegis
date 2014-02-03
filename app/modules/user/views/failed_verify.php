bf84da51c11c5878eb45e6ade4fecc0660a83827
Julio Taylor <julio@bigspring.co.uk>
2014-02-03 10:08:07 +0000

    clean of /verify markup
    #7

--- app/modules/user/views/failed_verify.php
+++ app/modules/user/views/failed_verify.php
@@ -1,4 +1,2 @@
-<div class="page-header">
-    <h1>Email Verification</h1>
-</div> <!-- /.page-header -->
+<h1>Email Verification</h1>
 <p>Sorry, we were unable to verify your email address.  Has your account already been verified?</p>
