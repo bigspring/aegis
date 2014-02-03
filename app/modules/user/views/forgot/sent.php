6e77713ed811f893d9adf054041fc84646b56b7c
Julio Taylor <julio@bigspring.co.uk>
2014-02-03 10:11:56 +0000

    #7 cleaned up users/sent

--- app/modules/user/views/forgot/sent.php
+++ app/modules/user/views/forgot/sent.php
@@ -1,24 +1,2 @@
-<div id="details" class="header-wrap">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="page-header">
-                    <h1>Password Reminder Sent</h1>
-                </div> <!-- /.page-header -->
-            </div> <!-- /.col-md-12 -->
-            <div class="col-md-3">
-            </div>
-        </div> <!-- /.row -->
-    </div>
-</div>
-<div class="container">
-        <div id="main">
-                <div id="content">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <p>We have sent you an email with a link to reset your password.  Please read this email and follow the instructions from there to reset your password and get logged in to the site.</p>                            
-                        </div>
-                    </div>  
-                </div> <!-- /#content -->
-        </div> <!-- /#main -->
-</div> <!-- /.container -->
+<h1>Password Reminder Sent</h1>
+<p>We have sent you an email with a link to reset your password.  Please read this email and follow the instructions from there to reset your password and get logged in to the site.</p>
