638713bd911e507958333c23830f8fa25a81d94b
Julio Taylor <julio@bigspring.co.uk>
2014-02-03 11:23:04 +0000

    improve default markup of /page

--- app/modules/pages/views/home.php
+++ app/modules/pages/views/home.php
@@ -1,1 +1,8 @@
-<h1>Welcome to the Machine</h1>
+<header class="page-header" role="banner">
+	<h1>Welcome to the Machine</h1>
+	<p class="lead">This is a lead sub-headline</p>	
+</header>
+
+<main role="main">
+	<p>You dreamed of a big star, he played a mean guitar, He always ate in the Steak Bar. He loved to drive in his Jaguar. So welcome to the machine.</p>
+</main>
