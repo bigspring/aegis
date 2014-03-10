2273d10de77edf3bd775bb7f59fe1683e3b832eb
korovacow <korova.cow@gmail.com>
2014-03-10 19:03:45 +0000

    #29 wip

--- app/modules/user/views/login/login.php
+++ app/modules/user/views/login/login.php
@@ -1,22 +1,99 @@
-<?php $this->load->view('notifications') // display the notitification partial ?>
-<div class="alert alert-warning">Access to this area is restricted.  Please log in to continue.</div>
-<h1>Login</h1>
-<?= form_open('', array('class' => 'form form-horizontal')); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
 
- <div class="form-group">
-	 <label class="col-sm-2 control-label" for="username">Username</label>
-	 <div class="col-sm-10">
-	 	<input class="form-control" type="text" value="" name="username" placeholder="Enter email Address"/>
-	 </div>
- </div>
- <div class="form-group">
-	 <label class="col-sm-2 control-label" for="password">Password</label>
-	 <div class="col-sm-10">
-	 	<input class="form-control" type="password" value="" name="password" placeholder="Enter password"/>
-	 </div>
- </div>
- 
- 
- 
- <?= form_submit(array('name' => 'submit', 'id' => 'submit', 'class' => 'btn btn-default', 'value' => 'Login')) ?>			
-<?= form_close() ?>						
+	<title>Signin Template for Bootstrap</title>
+
+	<!-- Bootstrap core CSS -->
+	<!-- <link href="../../dist/css/bootstrap.css" rel="stylesheet"> -->
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+
+	<!-- Custom styles for this template -->
+	<!-- <link href="signin.css" rel="stylesheet"> -->
+
+	<style>
+
+		body {
+			padding-top: 40px;
+			padding-bottom: 40px;
+			background-color: #eee;
+		}
+
+		.form-signin {
+			max-width: 330px;
+			padding: 15px;
+			margin: 0 auto;
+		}
+		.form-signin .form-signin-heading,
+		.form-signin .checkbox {
+			margin-bottom: 10px;
+		}
+		.form-signin .checkbox {
+			font-weight: normal;
+		}
+		.form-signin .form-control {
+			position: relative;
+			font-size: 16px;
+			height: auto;
+			padding: 10px;
+			-webkit-box-sizing: border-box;
+			-moz-box-sizing: border-box;
+			box-sizing: border-box;
+		}
+		.form-signin .form-control:focus {
+			z-index: 2;
+		}
+		.form-signin input[type="text"] {
+			margin-bottom: -1px;
+			border-bottom-left-radius: 0;
+			border-bottom-right-radius: 0;
+		}
+		.form-signin input[type="password"] {
+			margin-bottom: 10px;
+			border-top-left-radius: 0;
+			border-top-right-radius: 0;
+		}
+
+	</style>
+
+	<!-- Just for debugging purposes. Don't actually copy this line! -->
+	<!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+
+<div class="container">
+
+	<?php $this->load->view('notifications') // display the notitification partial ?>
+
+	<form class="form-signin" role="form" method="post">
+		<h2 class="form-signin-heading">Please sign in</h2>
+		<input type="text" class="form-control" placeholder="Email address" name="username" required autofocus>
+		<input type="password" class="form-control" placeholder="Password" name="password" required>
+		<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+	</form>
+
+</div> <!-- /container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+
+</body>
+</html>
